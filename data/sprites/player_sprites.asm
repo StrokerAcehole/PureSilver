85ea4996b6fe5bad440de02f7c791bba6db0ba2e
StrokerAcehole <146281185+StrokerAcehole@users.noreply.github.com>
2024-04-10 11:27:42 -0400

    Added Running Shoes

--- data/sprites/player_sprites.asm
+++ data/sprites/player_sprites.asm
@@ -1,6 +1,10 @@
-ChrisStateSprites:
-	db PLAYER_NORMAL,    SPRITE_CHRIS
-	db PLAYER_BIKE,      SPRITE_CHRIS_BIKE
-	db PLAYER_SURF,      SPRITE_SURF
-	db PLAYER_SURF_PIKA, SPRITE_SURFING_PIKACHU
-	db -1 ; end
+ ChrisStateSprites:
+ 	db PLAYER_BIKE,      SPRITE_CHRIS_BIKE
+ 	db PLAYER_SURF,      SPRITE_SURF
+ 	db PLAYER_SURF_PIKA, SPRITE_SURFING_PIKACHU
+	db PLAYER_RUN,       SPRITE_CHRIS_RUN
+ 	db -1 ; end
+
+ KrisStateSprites:
+	db PLAYER_RUN,       SPRITE_KRIS_RUN
+ 	db -1 ; end
